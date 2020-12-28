169893b701ea393ebc46fd7597fd7366d7fded6c
Ross Nelson <ross@simiancreative.com>
2020-12-28 16:30:21 -0500

    add serisci integration

--- bin/test.sh
+++ bin/test.sh
@@ -4,9 +4,11 @@ set -e
 
 list=`go list ./... | grep -v mocks | grep -v docs | grep -v errors`
 
-go test -cover $list
+go test -cover -coverprofile=coverage.out $list
 
-coverage=$(go tool cover -func=coverage.out)
+coverage=$(go tool cover -func coverage.out | grep total | awk '{print $3}')
+
+./bin/post-coverage.sh $coverage
 
 echo
-echo coverage: $(echo $coverage | grep total | awk '{print $3}')
+echo coverage: $coverage
