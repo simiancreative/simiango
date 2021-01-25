49deadb0efa0fc1154532f9053a4118614dd2606
Ross Nelson <ross@simiancreative.com>
2021-01-24 22:57:14 -0500

    add sqlx to the pg data package

--- data/pg/pg.go
+++ data/pg/pg.go
@@ -2,7 +2,11 @@ package pg
 
 import (
 	"context"
+	"database/sql"
 	"os"
+
+	_ "github.com/jackc/pgx/stdlib"
+	"github.com/jmoiron/sqlx"
 
 	"github.com/jackc/pgconn"
 	"github.com/jackc/pgx/v4"
@@ -11,6 +15,25 @@ import (
 
 var Ctx context.Context
 var C Conn
+var Cx ConnX
+
+type ConnX interface {
+	sqlx.Queryer
+
+	MapperFunc(func(string) string)
+	Rebind(string) string
+	Unsafe() *sqlx.DB
+	BindNamed(string, interface{}) (string, []interface{}, error)
+	NamedQuery(string, interface{}) (*sqlx.Rows, error)
+	NamedExec(string, interface{}) (sql.Result, error)
+	Select(interface{}, string, ...interface{}) error
+	Get(interface{}, string, ...interface{}) error
+	MustBegin() *sqlx.Tx
+	Beginx() (*sqlx.Tx, error)
+	MustExec(string, ...interface{}) sql.Result
+	Preparex(string) (*sqlx.Stmt, error)
+	PrepareNamed(string) (*sqlx.NamedStmt, error)
+}
 
 type Conn interface {
 	Begin(context.Context) (pgx.Tx, error)
@@ -24,6 +47,8 @@ type Conn interface {
 
 func init() {
 	addr := os.Getenv("DATABASE_URL")
+
 	Ctx = context.Background()
 	C, _ = pgxpool.Connect(Ctx, addr)
+	Cx, _ = sqlx.Connect("pgx", addr)
 }
--- data/pg/pg_test.go
+++ data/pg/pg_test.go
@@ -7,10 +7,14 @@ import (
 )
 
 var ConnMock *m.Conn
+var ConnXMock *m.ConnX
 
 func init() {
 	ConnMock = &m.Conn{}
 	C = ConnMock
+
+	ConnXMock = &m.ConnX{}
+	Cx = ConnXMock
 }
 
 func TestQuery(t *testing.T) {
@@ -24,4 +28,17 @@ func TestQuery(t *testing.T) {
 	C.Query(Ctx, query, param)
 
 	ConnMock.AssertExpectations(t)
+}
+
+func TestQueryX(t *testing.T) {
+	query := "select * from widgets where id=$1"
+	param := 42
+
+	ConnXMock.
+		On("Queryx", query, param).
+		Return(nil, nil)
+
+	Cx.Queryx(query, param)
+
+	ConnXMock.AssertExpectations(t)
 }
