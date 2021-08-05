9cf6342661ac8ef296f3209e22ddadab6f67fbf2
Ross Nelson <axcess1@me.com>
2021-08-05 12:40:51 -0400

    Add coveralls to ci

--- bin/test.sh
+++ bin/test.sh
@@ -2,9 +2,12 @@
 
 set -e
 
+go install github.com/mattn/goveralls@latest
+
 list=`go list ./... | grep -v mocks | grep -v docs | grep -v errors`
 
 go test -cover -coverprofile=coverage.out $list
+goveralls -coverprofile=coverage.out -repotoken=${COVERALLS_TOKEN}
 
 coverage=$(go tool cover -func coverage.out | grep total | awk '{print $3}')
 
