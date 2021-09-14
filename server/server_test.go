3f90e8292e01bd73ccca5cfb6725af3c7e1590b7
Ross Nelson <axcess1@me.com>
2021-09-14 14:14:32 -0400

    demonstrate service result nils

--- server/server_test.go
+++ server/server_test.go
@@ -3,22 +3,33 @@ package server
 import (
 	"testing"
 
+	"github.com/simiancreative/simiango/service"
 	"github.com/stretchr/testify/assert"
 )
 
-type _s struct{}
-
-func (s _s) Result() (interface{}, error) {
-	return response()
+type _s struct {
+	result interface{}
 }
 
-func response() (*string, error) {
-	return nil, nil
+func (s _s) Result() (interface{}, error) {
+	return s.result, nil
 }
 
 func TestServiceResult(t *testing.T) {
-	result, err := serviceResult(_s{})
+	result, err := serviceResult(_s{result: nil})
 
-	assert.Nil(t, result)
-	assert.Nil(t, err)
+	assert.Equal(t, result, nil)
+	assert.Equal(t, err, (*service.ResultError)(nil))
+}
+
+func TestServiceResultNilPointer(t *testing.T) {
+	var result interface{}
+	var str *string
+
+	result = str
+
+	result, err := serviceResult(_s{result: result})
+
+	assert.Equal(t, result, (*string)(nil))
+	assert.Equal(t, err, (*service.ResultError)(nil))
 }
