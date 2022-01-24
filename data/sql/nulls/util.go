60fb309780ac60ef48a833d33c026e068a6ca1c7
Ross Nelson <axcess1@me.com>
2022-01-24 12:22:22 -0500

    change the nulls float conversion logic

--- data/sql/nulls/util.go
+++ data/sql/nulls/util.go
@@ -1,13 +1,17 @@
 package nulls
 
 import (
-	"encoding/binary"
-	"math"
+	// "fmt"
+	"strconv"
+	// "encoding/binary"
+	// "math"
 )
 
 func Float64fromUint8s(i []byte) float64 {
-	bits := binary.LittleEndian.Uint64(i)
-	float := math.Float64frombits(bits)
+	const bitSize = 64 // Don't think about it to much. It's just 64 bits.
+	float, _ := strconv.ParseFloat(string(i), bitSize)
+
+	// bits := binary.LittleEndian.Uint64(i)
+	// float := math.Float64frombits(bits)
 	return float
 }
-
