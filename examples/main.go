f0ab8038e3dcf30860374012f15656e2abaecba4
Ross Nelson <axcess1@me.com>
2022-01-27 15:02:07 -0500

    fix example

--- examples/main.go
+++ examples/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	_ "github.com/simiancreative/simiango/config"
-	_ "github.com/simiancreative/simiango/examples/docs"
 
 	_ "github.com/simiancreative/simiango/data/mssql"
 	_ "github.com/simiancreative/simiango/data/mysql"
@@ -22,17 +21,8 @@ import (
 	_ "github.com/simiancreative/simiango/examples/services/rabbit"
 )
 
-// @title Sample API
-// @version 1.0
-// @description This is a sample service for simian go
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-// @host localhost:8080
-// @BasePath /
-
 func main() {
 	server.EnableHealthCheck()
-	server.EnableSwagger()
 	server.SetCORS()
 
 	server.AddServices(service.Collection{
