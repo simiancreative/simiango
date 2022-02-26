36f5b737a2d126e00623b426fd1370baa77dcae7
Ross Nelson <axcess1@me.com>
2022-02-26 16:25:46 -0500

    Add content response error property

--- service/content_response_test.go
+++ service/content_response_test.go
@@ -13,7 +13,21 @@ func TestToContentResponse(t *testing.T) {
 		Size:  25,
 	})
 
+	assert.NoError(t, content.Error)
 	assert.Equal(t, 200, content.TotalPages)
 	assert.Equal(t, true, content.First)
+	assert.Equal(t, false, content.Last)
+}
+
+func TestToContentResponseNotSlice(t *testing.T) {
+	content := ToContentResponse(3, ContentResponseMeta{
+		Total: 5000,
+		Page:  1,
+		Size:  25,
+	})
+
+	assert.Error(t, content.Error)
+	assert.Equal(t, 0, content.TotalPages)
+	assert.Equal(t, false, content.First)
 	assert.Equal(t, false, content.Last)
 }
