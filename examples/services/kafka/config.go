2c3666b57df08d02fbdc67532ae390f38a71ec1d
Ross Nelson <axcess1@me.com>
2022-03-02 10:44:53 -0500

    IOT-6649: Correct service selection and add message details to headers and params (#17)

--- examples/services/kafka/config.go
+++ examples/services/kafka/config.go
@@ -1,15 +1,22 @@
 package kafka
 
 import (
-	//_ "consumers/notifications/templates/access_method"
-	"os"
-
 	"github.com/simiancreative/simiango/messaging/kafka"
 	"github.com/simiancreative/simiango/service"
 )
 
+// Send a message manually
+//
+// docker exec -it simiango-kafka-1 bash
+// kafka-console-producer.sh --broker-list kafka:9092 --topic decoded
+//
+// at the prompt enter you message and hit return, the service will be invoked
+// and service handling will be logged
+
 var Config = service.Config{
-	Key:           os.Getenv("KAFKA_READER_TOPIC"),
+	// Matched to KAFKA_HANDLER when selecting the runtime service that will
+	// process messages
+	Key:           "example-handler",
 	BuildMessages: Build,
 }
 
--- messaging/kafka/builder.go
+++ messaging/kafka/builder.go
@@ -1,6 +1,9 @@
 package kafka
 
 import (
+	"fmt"
+
+	kafka "github.com/segmentio/kafka-go"
 	"github.com/simiancreative/simiango/meta"
 	"github.com/simiancreative/simiango/service"
 )
@@ -8,9 +11,50 @@ import (
 func buildService(
 	requestID meta.RequestId,
 	config service.Config,
-	body service.RawBody,
+	message kafka.Message,
 ) (service.MessageTPL, error) {
-	s, err := config.BuildMessages(requestID, service.RawHeaders{}, body, service.RawParams{})
+	headers := service.RawHeaders{}
+	params := service.RawParams{}
+
+	for _, header := range message.Headers {
+		headers = append(headers, service.ParamItem{
+			Key:   header.Key,
+			Value: string(header.Value),
+		})
+	}
+
+	params = append(params, service.ParamItem{
+		Key:   "topic",
+		Value: message.Topic,
+	})
+
+	params = append(params, service.ParamItem{
+		Key:   "partition",
+		Value: fmt.Sprint(message.Partition),
+	})
+
+	params = append(params, service.ParamItem{
+		Key:   "offset",
+		Value: fmt.Sprint(message.Offset),
+	})
+
+	params = append(params, service.ParamItem{
+		Key:   "high_water_mark",
+		Value: fmt.Sprint(message.HighWaterMark),
+	})
+
+	params = append(params, service.ParamItem{
+		Key:   "time",
+		Value: fmt.Sprint(message.Time.Unix()),
+	})
+
+	s, err := config.BuildMessages(
+		requestID,
+		headers,
+		message.Value,
+		params,
+	)
+
 	if err == nil {
 		return s, nil
 	}
