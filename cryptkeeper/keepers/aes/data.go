922eb0bd6fed1cc0803a0e942dd1960fc132e7a6
Ross Nelson <axcess1@me.com>
2024-12-30 22:43:15 -0600

    add tests

--- cryptkeeper/keepers/aes/data.go
+++ cryptkeeper/keepers/aes/data.go
@@ -13,17 +13,22 @@ type Data struct {
 }
 
 func (e Data) Read(p []byte) (n int, err error) {
-	data, err := json.Marshal(e)
+	// implement a read method that streams Datas json representation
+
+	// convert the Data struct to json
+	b, err := json.Marshal(e)
 	if err != nil {
-		return
+		return 0, err
 	}
 
-	if e.pos >= len(data) {
-		return 0, io.EOF
-	}
-
-	n = copy(p, data[e.pos:])
+	// copy the json to the byte slice
+	n = copy(p, b[e.pos:])
 	e.pos += n
+
+	// if we have read all the json, return EOF
+	if e.pos >= len(b) {
+		return n, io.EOF
+	}
 
 	return n, nil
 }
