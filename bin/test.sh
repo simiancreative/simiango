e38fbc72fcb10e1ee9f1db9448dc46273dfa2b12
Ross Nelson <axcess1@me.com>
2024-12-31 21:35:25 -0600

    add codecov

--- bin/test.sh
+++ bin/test.sh
@@ -4,13 +4,10 @@ set -e
 
 export GOEXPERIMENT=nocoverageredesign
 
-go install github.com/mattn/goveralls@latest
-
 list=`go list ./... | grep -v mocks | grep -v docs | grep -v errors | grep -v examples`
 
 go test -coverpkg=./... -race -covermode=atomic -coverprofile=coverage.out $list
-goveralls -coverprofile=coverage.out -repotoken=${COVERALLS_TOKEN} \
-  || echo 'not posted to coveralls'
+go tool cover -func coverage.out > coverage-report.txt
 
 coverage=$(go tool cover -func coverage.out | grep total | awk '{print $3}')
 
