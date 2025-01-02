9082cb0514701157843c23e8c64d24667ee827f4
Ross Nelson <axcess1@me.com>
2025-01-02 15:58:14 -0600

    file name

--- bin/test.sh
+++ bin/test.sh
@@ -8,7 +8,7 @@ go install github.com/jstemmer/go-junit-report/v2@latest
 
 list=`go list ./... | grep -v mocks | grep -v docs | grep -v errors | grep -v examples`
 
-go test -v -coverpkg=./... -race -covermode=atomic -coverprofile=coverage.out 2>&1 $list | go-junit-report -set-exit-code > report.xml
+go test -v -coverpkg=./... -race -covermode=atomic -coverprofile=coverage.out 2>&1 $list | go-junit-report -set-exit-code > junit.xml
 
 coverage=$(go tool cover -func coverage.out | grep total | awk '{print $3}')
 
