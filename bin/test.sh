a96b14303df1225c2cb88a8cfa6188b288c0e73e
Ross Nelson <axcess1@me.com>
2025-01-02 17:02:32 -0500

    Add codecov tests reporting (#43)

--- bin/test.sh
+++ bin/test.sh
@@ -4,10 +4,11 @@ set -e
 
 export GOEXPERIMENT=nocoverageredesign
 
+go install github.com/jstemmer/go-junit-report/v2@latest
+
 list=`go list ./... | grep -v mocks | grep -v docs | grep -v errors | grep -v examples`
 
-go test -coverpkg=./... -race -covermode=atomic -coverprofile=coverage.out $list
-go tool cover -func coverage.out > coverage-report.txt
+go test -v -coverpkg=./... -race -covermode=atomic -coverprofile=coverage.out 2>&1 $list | go-junit-report -set-exit-code > junit.xml
 
 coverage=$(go tool cover -func coverage.out | grep total | awk '{print $3}')
 
