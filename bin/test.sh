b5bff38c5bacb90b55c2ffbe3b9a84c41f905d70
Ross Nelson <axcess1@me.com>
2025-03-03 09:21:55 -0500

    expose test log

--- bin/test.sh
+++ bin/test.sh
@@ -8,7 +8,11 @@ go install github.com/jstemmer/go-junit-report/v2@latest
 
 list=`go list ./... | grep -v mocks | grep -v docs | grep -v errors | grep -v examples`
 
-go test -v -coverpkg=./... -race -covermode=atomic -coverprofile=coverage.out 2>&1 $list | go-junit-report -set-exit-code > junit.xml
+go test -v -coverpkg=./... -race -covermode=atomic -coverprofile=coverage.out 2>&1 $list > test.log
+
+cat test.log
+
+cat test.log | go-junit-report -set-exit-code > junit.xml
 
 coverage=$(go tool cover -func coverage.out | grep total | awk '{print $3}')
 
